affbe2de18fbb701b3246ed8c9e8b26ad88921a0
rodionhatskevich <rodionhatskevich@gmail.com>
2022-09-17 23:08:43 +0200

    fix error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,16 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
+	root: true,
+	extends: "@react-native-community",
 	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-	},
-	rules: {
-		"no-prototype-builtins": "off",
-		"@typescript-eslint/ban-types": "off",
-		"@typescript-eslint/explicit-function-return-type": "off",
-		"@typescript-eslint/explicit-module-boundary-types": "off",
-		"@typescript-eslint/no-explicit-any": "warn",
-		"@typescript-eslint/no-empty-function": "off",
-		"@typescript-eslint/no-non-null-assertion": "off",
-		"@typescript-eslint/no-unused-vars": [
-			"warn",
-			{
-				argsIgnorePattern: "^_",
-			},
-		],
-		"linebreak-style": ["error", "unix"],
-		quotes: ["error", "double"],
-		semi: ["error", "always"],
-	},
 	plugins: ["@typescript-eslint"],
-	extends: [
-		"@react-native-community",
-		"airbnb-typescript",
-		"prettier",
-		"prettier/@typescript-eslint",
-		"prettier/react",
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			rules: {
+				"@typescript-eslint/no-shadow": ["error"],
+				"no-shadow": "off",
+				"no-undef": "off",
+			},
+		},
 	],
 };
